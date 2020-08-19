ed4aa70499c9fba2c70bfba5f7e032ad8f609799
sayachang_bot <joshuaarthurelcaro@gmail.com>
2020-08-19 21:47:22 +0900

    testing oreorepanini

---
+++ Assets/Custom Pass/OreOrePanini Pass/OreOrePaniniPass.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+public class OreOrePaniniPass : CustomPass
+{
+    [Range(0.01f, 100)]
+    public float d = 1;
+
+    [SerializeField, HideInInspector]
+    Shader shader;
+    Material material;
+    MaterialPropertyBlock materialProperties;
+    ShaderTagId[] shaderTags;
+    RTHandle rtBuffer;
+    protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd)
+    {
+        shader = Shader.Find("FullScreen/OreOrePaniniPass");
+        material = CoreUtils.CreateEngineMaterial(shader);
+        materialProperties = new MaterialPropertyBlock();
+        shaderTags = new ShaderTagId[3]
+        {
+            new ShaderTagId("Forward"),
+            new ShaderTagId("ForwardOnly"),
+            new ShaderTagId("SRPDefaultUnlit"),
+        };
+
+        rtBuffer = RTHandles.Alloc(
+            Vector2.one, TextureXR.slices, dimension: TextureXR.dimension,
+            colorFormat: GraphicsFormat.B10G11R11_UFloatPack32,
+            useDynamicScale: true, name: "RTBuffer"
+        );
+    }
+    void DrawMeshes(ScriptableRenderContext renderContext, CommandBuffer cmd, HDCamera hdCamera, CullingResults cullingResult)
+    {
+        var result = new RendererListDesc(shaderTags, cullingResult, hdCamera.camera)
+        {
+            rendererConfiguration = PerObjectData.LightProbe | PerObjectData.LightProbeProxyVolume | PerObjectData.Lightmaps,
+            renderQueueRange = RenderQueueRange.all,
+            sortingCriteria = SortingCriteria.BackToFront,
+            excludeObjectMotionVectors = false,
+            layerMask = 0,
+        };
+
+        CoreUtils.SetRenderTarget(cmd, rtBuffer, ClearFlag.Color);
+        HDUtils.DrawRendererList(renderContext, cmd, RendererList.Create(result));
+    }
+    protected override void Execute(ScriptableRenderContext renderContext, CommandBuffer cmd, HDCamera camera, CullingResults cullingResult)
+    {
+        DrawMeshes(renderContext, cmd, camera, cullingResult);
+        SetCameraRenderTarget(cmd);
+
+        materialProperties.SetFloat("_D", d);
+
+    CoreUtils.DrawFullScreen(cmd, material, materialProperties, shaderPassId: 0);
+    }
+    protected override void Cleanup()
+    {
+        CoreUtils.Destroy(material);
+        rtBuffer.Release();
+    }
+}
---
+++ Assets/Custom Pass/OreOrePanini Pass/OreOrePaniniPass.shader
@@ -0,0 +1,60 @@
+﻿Shader "FullScreen/OreOrePaniniPass"
+{
+    HLSLINCLUDE
+    #pragma vertex Vert
+    #pragma target 4.5
+    #pragma only_renderers d3d11 ps4 xboxone vulkan metal switch
+    #include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/CustomPass/CustomPassCommon.hlsl"
+
+    float _D;
+    float2 oreorePanini(float2 uv, float d)
+    {
+        float k = uv.x * uv.x / ((d + 1) * (d + 1));
+        float dscr = k * k * d * d - (k + 1) * (k * d * d - 1);
+        float clon = (-k * d + sqrt(dscr)) / (k + 1);
+        float S = (d + 1) / (d + clon);
+        float lon = atan2(uv.x, S * clon);
+        float lat = atan2(uv.y, S);
+
+        float clat = cos(lat);
+        return float2(sin(lon) * clat, sin(lat));
+        //cos(lon) * clat;
+    }
+
+    float4 FullScreenPass(Varyings varyings) : SV_Target
+    {
+        UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(varyings);
+        float depth = LoadCameraDepth(varyings.positionCS.xy);
+        PositionInputs posInput = GetPositionInput(varyings.positionCS.xy, _ScreenSize.zw, depth, UNITY_MATRIX_I_VP, UNITY_MATRIX_V);
+        float3 viewDirection = GetWorldSpaceNormalizeViewDir(posInput.positionWS);
+        float4 color = float4(0.0, 0.0, 0.0, 0.0);
+        if (_CustomPassInjectionPoint != CUSTOMPASSINJECTIONPOINT_BEFORE_RENDERING)
+            color = float4(CustomPassSampleCameraColor(posInput.positionNDC.xy, 0), 1);
+
+        float2 uv = posInput.positionNDC.xy;
+        float2 nuv = oreorePanini(uv, _D);
+        color.rgb = CustomPassSampleCameraColor(nuv, 0);
+
+        return color;
+    }
+
+    ENDHLSL
+
+    SubShader
+    {
+        Pass
+        {
+            Name "OreOrePanini Pass"
+
+            ZWrite Off
+            ZTest Always
+            Blend SrcAlpha OneMinusSrcAlpha
+            Cull Off
+
+            HLSLPROGRAM
+                #pragma fragment FullScreenPass
+            ENDHLSL
+        }
+    }
+    Fallback Off
+}
