db42ff926e3f4c2f727876e3c6a7d073853e5939
sayachang_bot <joshuaarthurelcaro@gmail.com>
2020-09-13 05:22:01 +0900

    added Azayaka Pass (W.I.P)

---
+++ Assets/Custom Pass/Azayaka Pass/AzayakaPass.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.Rendering;
+using UnityEngine.Experimental.Rendering;
+public class AzayakaPass : CustomPass
+{
+    [Range(-0.1f, 0.1f)]
+    public float chromaAdd = 0.05f;
+    [Range(0, 2)]
+    public float chromaMul = 1.3f;
+    [Range(0, 1)]
+    public float valueAdd = 0.33f;
+    [Range(0, 2)]
+    public float valueMul = 2.0f;
+    [Range(0, 1)]
+    public float border = 0.5f;
+    [SerializeField, HideInInspector]
+    Shader rainShader;
+
+    Material fullscreenMaterial;
+    MaterialPropertyBlock materialProperties;
+    ShaderTagId[] shaderTags;
+    RTHandle rtBuffer;
+    protected override void Setup(ScriptableRenderContext renderContext, CommandBuffer cmd)
+    {
+        rainShader = Shader.Find("FullScreen/AzayakaPass");
+        fullscreenMaterial = CoreUtils.CreateEngineMaterial(rainShader);
+        materialProperties = new MaterialPropertyBlock();
+
+        // List all the materials that will be replaced in the frame
+        shaderTags = new ShaderTagId[3]
+        {
+            new ShaderTagId("Forward"),
+            new ShaderTagId("ForwardOnly"),
+            new ShaderTagId("SRPDefaultUnlit"),
+        };
+
+        rtBuffer = RTHandles.Alloc(
+            Vector2.one, TextureXR.slices, dimension: TextureXR.dimension,
+            colorFormat: GraphicsFormat.B10G11R11_UFloatPack32,
+            useDynamicScale: true, name: "Azayaka Buffer"
+        );
+    }
+    void DrawOutlineMeshes(ScriptableRenderContext renderContext, CommandBuffer cmd, HDCamera hdCamera, CullingResults cullingResult)
+    {
+        var result = new RendererListDesc(shaderTags, cullingResult, hdCamera.camera)
+        {
+            // We need the lighting render configuration to support rendering lit objects
+            rendererConfiguration = PerObjectData.LightProbe | PerObjectData.LightProbeProxyVolume | PerObjectData.Lightmaps,
+            renderQueueRange = RenderQueueRange.all,
+            sortingCriteria = SortingCriteria.BackToFront,
+            excludeObjectMotionVectors = false,
+            layerMask = 0,
+        };
+
+        CoreUtils.SetRenderTarget(cmd, rtBuffer, ClearFlag.Color);
+        HDUtils.DrawRendererList(renderContext, cmd, RendererList.Create(result));
+    }
+    protected override void Execute(ScriptableRenderContext renderContext, CommandBuffer cmd, HDCamera hdCamera, CullingResults cullingResult)
+    {
+        DrawOutlineMeshes(renderContext, cmd, hdCamera, cullingResult);
+
+        SetCameraRenderTarget(cmd);
+
+        materialProperties.SetTexture("_BufferTex", rtBuffer);
+        materialProperties.SetFloat("_ChromaAdd", chromaAdd);
+        materialProperties.SetFloat("_ChromaMul", chromaMul);
+        materialProperties.SetFloat("_ValueAdd", valueAdd);
+        materialProperties.SetFloat("_ValueMul", valueMul);
+        materialProperties.SetFloat("_Border", border);
+        CoreUtils.DrawFullScreen(cmd, fullscreenMaterial, materialProperties, shaderPassId: 0);
+    }
+    protected override void Cleanup()
+    {
+        CoreUtils.Destroy(fullscreenMaterial);
+        rtBuffer.Release();
+    }
+}
---
+++ Assets/Custom Pass/Azayaka Pass/AzayakaPass.shader
@@ -0,0 +1,88 @@
+﻿Shader "FullScreen/AzayakaPass"
+{
+HLSLINCLUDE
+#pragma vertex Vert
+#pragma target 4.5
+#pragma only_renderers d3d11 ps4 xboxone vulkan metal switch
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/RenderPass/CustomPass/CustomPassCommon.hlsl"
+
+    TEXTURE2D_X(_BufferTex);
+    float _ChromaAdd;
+    float _ChromaMul;
+    float _ValueAdd;
+    float _ValueMul;
+    float _Border;
+
+    float3 rgb2hsv(float3 c)
+    {
+        float4 K = float4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+        float4 p = lerp(float4(c.bg, K.wz), float4(c.gb, K.xy), step(c.b, c.g));
+        float4 q = lerp(float4(p.xyw, c.r), float4(c.r, p.yzx), step(p.x, c.r));
+
+        float d = q.x - min(q.w, q.y);
+        float e = 1.0e-10;
+        return float3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+    }
+
+    float3 hsv2rgb(float3 c)
+    {
+        float4 K = float4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+        float3 p = abs(frac(c.xxx + K.xyz) * 6.0 - K.www);
+        return c.z * lerp(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    }
+
+    float3 azayaka(float3 col) {
+        float valueAdd = _ValueAdd;
+        float valueMul = _ValueMul;
+
+        float lum = dot(col, float3(0.21, 0.72, 0.07));
+        float3 hsv = rgb2hsv(col);
+        
+        hsv.y = (hsv.y + _ChromaAdd) * _ChromaMul;
+        hsv.z += valueMul * (lum - .5 + valueAdd);
+
+        return hsv2rgb(hsv);
+    }
+
+    float4 FullScreenPass(Varyings varyings) : SV_Target
+    {
+        UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(varyings);
+        float depth = LoadCameraDepth(varyings.positionCS.xy);
+        PositionInputs posInput = GetPositionInput(varyings.positionCS.xy, _ScreenSize.zw, depth, UNITY_MATRIX_I_VP, UNITY_MATRIX_V);
+        float3 viewDirection = GetWorldSpaceNormalizeViewDir(posInput.positionWS);
+        float4 color = float4(0.0, 0.0, 0.0, 0.0);
+
+        // Load the camera color buffer at the mip 0 if we're not at the before rendering injection point
+        if (_CustomPassInjectionPoint != CUSTOMPASSINJECTIONPOINT_BEFORE_RENDERING)
+            color = float4(CustomPassSampleCameraColor(posInput.positionNDC.xy, 0), 1);
+
+        float2 uv = posInput.positionNDC.xy;
+        float3 col = azayaka(color.rgb);
+        
+        col = lerp(color.rgb, col, step(0., uv.x - _Border));
+        if (abs(uv.x - _Border) < .005 && _Border > 0 && _Border < 1) col = float3(1., 1., 0.);
+        
+        color.rgb = col;
+        return color;
+    }
+
+        ENDHLSL
+
+        SubShader
+    {
+        Pass
+        {
+            Name "Azayaka Pass"
+
+            ZWrite Off
+            ZTest Always
+            Blend SrcAlpha OneMinusSrcAlpha
+            Cull Off
+
+            HLSLPROGRAM
+                #pragma fragment FullScreenPass
+            ENDHLSL
+        }
+    }
+    Fallback Off
+}
